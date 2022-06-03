1b8ba706000cc1524c58ac117789b3080079f0b6
POrellanaUSM <72226229+POrellanaUSM@users.noreply.github.com>
2022-06-03 18:43:39 -0400

    Limpiando Mi branch

--- Tarea-2-Grupo7/hash_password.php
+++
@@ -1,8 +0,0 @@
-<?php
-function hash_password($password) {
-
- $options = array('cost' => 12);
- $password_hashed = password_hash($password, PASSWORD_BCRYPT, $options);
- return $password_hashed;
-}
-?>
